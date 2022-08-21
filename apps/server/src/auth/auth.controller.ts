9452efb088138ac6547202c382eb885ecf474dc7
valfur03 <valentin.furmanek@gmail.com>
2022-08-21 11:30:31 +0200

    docs: document the Auth controller

--- apps/server/src/auth/auth.controller.ts
+++ apps/server/src/auth/auth.controller.ts
@@ -4,20 +4,40 @@ import { AuthService } from './auth.service';
 import { StateData } from './interfaces/state-data.interface';
 import { NestRedirection } from '@alekol/data';
 
+/**
+ * The maximum number of iterations to generate a state.
+ * @constant
+ * @default
+ */
 const MAX_ITERATIONS = 20;
 
+/**
+ * /auth
+ */
 @Controller('auth')
 export class AuthController {
 	constructor(private authService: AuthService, private registrationService: RegistrationService) {}
 
+	/**
+	 * GET /register
+	 * Redirects to the next service which needs the user's authorization.
+	 * Creates a state parameter if needed.
+	 * @param {string} state The state of the authentication process.
+	 * @return {NestRedirection} The URL to the next service.
+	 * @throws {BadRequestException} The state must exist in the database if provided.
+	 * @throws {GatewayTimeoutException} The state cannot be a duplicate.
+	 */
 	@Get('register')
 	@Redirect()
 	register(@Query('state') state: string): NestRedirection {
 		let data: StateData | null = null;
 		if (state) {
+			// fetch the state's associated data
 			data = this.registrationService.fetchStateData(state);
 			if (data === null) throw new BadRequestException();
 		} else {
+			// generates a new state
+			// protect from timeouts with the MAX_ITERATIONS constant
 			let i = 0;
 			while (data === null) {
 				if (i++ >= MAX_ITERATIONS) throw new GatewayTimeoutException();
@@ -27,6 +47,17 @@ export class AuthController {
 		return { url: this.registrationService.getNextServiceURL(data) };
 	}
 
+	/**
+	 * GET /42
+	 * Authenticate the user with the authorization code and save his id and login.
+	 * @param {string} code The 42's authorization code.
+	 * @param {string} state The state of the authentication process.
+	 * @return {Promise<NestRedirection>} The URL to the next service.
+	 * @throws {BadRequestException} The state must exist in the database.
+	 * @throws {InternalServerErrorException} 42 could not issue an access token using the code.
+	 * @throws {InternalServerErrorException} 42 could not give information about the access token's owner.
+	 * @todo possible security issue: prevent setting id or login again with a second request.
+	 */
 	@Get('42')
 	@Redirect()
 	async authWith42(@Query('code') code: string, @Query('state') state: string): Promise<NestRedirection> {
