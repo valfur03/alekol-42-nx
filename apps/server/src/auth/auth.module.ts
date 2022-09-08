8f925cc24537facf8003fb56d2b576ad2020db46
valfur03 <valentin.furmanek@gmail.com>
2022-09-08 21:51:18 +0200

    fix: add StatesService to AuthModule

--- apps/server/src/auth/auth.module.ts
+++ apps/server/src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { RegistrationService } from '../registration/registration.service';
 import { AuthService } from './auth.service';
+import {StatesService} from '../states/states.service';
 
 @Module({
 	controllers: [AuthController],
-	providers: [AuthService, RegistrationService],
+	providers: [AuthService, RegistrationService, StatesService],
 })
 export class AuthModule {}
